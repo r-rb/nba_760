d1c4f699c1f8372bd24e7ac75dcec9611a1f46e2
Blair <blairacox@gmail.com>
2018-09-01 16:02:07 +1200

    Add player table generator

--- generate_tables.py
+++ generate_tables.py
@@ -112,10 +112,11 @@ def player_table():
     pdc_dict['active_start'] = []
     pdc_dict['active_end'] = []
 
+    #print(df.loc[df['player_id'] == 2747])
     count = 0
     removed_keys = []
     names = []
-    for i in range(0, 50000):
+    for i in range(0, len(df)):
         if not(count% 100):
             print(count)
 
@@ -124,10 +125,10 @@ def player_table():
             years = players[df.iloc[i]['player_id']]
             if df.iloc[i]['player_id'] not in pdc_dict: 
                 pdc_dict[df.iloc[i]['player_id']] = []
+                pdc_dict['active_start'].append(years['active_start'])
+                pdc_dict['active_end'].append(years['active_end'])
                 for c in columns:
                     pdc_dict[c[1]].append(df.iloc[i][c[2]])
-                    pdc_dict['active_start'].append(years['active_start'])
-                    pdc_dict['active_end'].append(years['active_end'])
         except KeyError as e:
             names.append(df.iloc[i]['player_name'])
             removed_keys.append(df.iloc[i]['player_id'])
